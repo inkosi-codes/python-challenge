74880a261956264a95be81e26324ff125ebd26f0
unknown <kiwwanne@gmail.com>
2020-10-24 16:07:55 -0500

    Updated formula for Profit/Losses change and Grestest (Increase & Decrease)

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,6 +10,11 @@ import pandas
 import sys
 import io
 
+# Create global list to hold our change information later while iterating through the Prpfit/Losses column
+orignal_pf = []
+profit = []
+profit_change = []
+
 # Declared global variables for use outside of functions
 #-----------------------------------------------------------
 working_dir = _dir.getcwd() # Used to get working directory of the main.py script for pulling data and saving output
@@ -64,19 +69,30 @@ def show_results(x, t, a, inc_date, inc, dec_date, dec):
 def process_csv(csv):
     rdr = pandas.read_csv(csv)
     csv_header = rdr.columns.values # Stores Header
-    head = rdr["Profit/Losses"].head()[0]
-    tail = rdr["Profit/Losses"].tail()[85]
-
     x = rdr["Date"].count()
     val = rdr["Profit/Losses"].values
-    t = val.sum() # Sum of profits and losses during the financial time period
-    a = round((tail - head) / x, 2)
-    inc = val.max()
-    inc_date = rdr.loc[rdr["Profit/Losses"] == inc, "Date"].values[0] # Uses the (inc) variable to match column "Profit/Losses" to the corresponding "Date" field 
-    dec = val.min()
-    dec_date = rdr.loc[rdr["Profit/Losses"] == dec, "Date"].values[0] # Uses the (dec) variable to match column "Profit/Losses" to the corresponding "Date" field 
-    
-    show_results(x, t, a, inc_date, inc, dec_date, dec) # Passes all populated variables to func (show_results)
+    t = val.sum()# Sum of profits and losses during the financial time period
+
+    #----------------------------------------------------------------------------------
+    # Iterate through our val object containing the values listed in our [Profit/Losses] 
+    # column and add orig values to our (original_pf) list and (profit) list. We will use
+    # the (profit) list to make our changes for calculations
+    #----------------------------------------------------------------------------------
+    for row in val:
+        orignal_pf.append(row)
+        profit.append(int(row))
+    for i in range(len(profit)-1):
+        profit_change.append(profit[i+1]-profit[i])
+
+    a = round(sum(profit_change)/len(profit_change),2)
+    inc = max(profit_change)
+    dec = min(profit_change)
+    inc_date = profit_change.index(max(profit_change))+1 # Provides the corresponding index for the max change time period
+    dec_date = profit_change.index(min(profit_change))+1# Provides the corresponding index for the min change time period
+    inc_date = rdr.loc[rdr["Profit/Losses"] == orignal_pf[inc_date], "Date"].values[0]# Uses the (orignal_pf) indexed list to match column "Date" based upon the profit change for the value of greatest increase 
+    dec_date = rdr.loc[rdr["Profit/Losses"] == orignal_pf[dec_date], "Date"].values[0]# Uses the (orignal_pf) indexed list to match column "Date" based upon the profit change for the value of greatest decrease  
+    show_results(x, t, a, inc_date, inc, dec_date, dec)
+
 
 #----------------------------------------------------------------------
 # The (getCsvFile) func uses the "working_dir" variable to identify 
